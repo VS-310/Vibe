aed857e114efdeede9287d465ee55d158132c77e
VS-310 <asnvf310@gmail.com>
2025-03-03 20:55:50 +0530

    03mar 08:55pm

---
+++ 10todoContextLocal/src/contexts/TodoContext.js
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        // { id: 1, text: "Learn React", completed: false},
+        // { id: 2, text: "Learn Firebase", completed: false},
+        // { id: 3, text: "Learn GraphQL", completed: false},
+    ],
+    addTodo: (todo) => {},
+    removeTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleTodo: (id) => {},
+});
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+
+export const TodoProvider = TodoContext.Provider;
